610153ebe0d62758375d5d59ecd7439a9715e362
PAVEL <pavelfedotov1995@icloud.com>
2025-12-01 19:08:17 +0200

    task_02 done

--- main.go
+++ main.go
@@ -7,21 +7,44 @@ import (
 )
 
 func main() {
+	////////////////////////////////
+	//   # Task 1: Basic Setup   //
+	//////////////////////////////
+
 	// The below "if" function check input arguments and save the text and banner name.
 
 	if len(os.Args) < 2 { // Check for empty input.
 		fmt.Println("ERROR\n", "Example use: go run main.go [TEXT] [BANNER NAME]")
-	} else if len(os.Args) == 2 { // if input arguments is only [TEXT] then the banner style will be "standart.txt" automaticly.
-		banner := "standart"
-		fmt.Println(banner)
+		return
+	}
+	argStr := os.Args[1]
+	var banner string
+
+	if len(os.Args) == 2 { // if input arguments is only [TEXT] then the banner style will be "standart.txt" automaticly.
+		banner = "standart"
 	} else if len(os.Args) == 3 {
-		banner := os.Args[2] // Here we save the name of a banner style. User should print "standard" or "shadow" or "thinkertoy".
-		fmt.Println(banner)
+		banner = strings.ToLower(os.Args[2]) // Here we save the name of a banner style. User should print "standard" or "shadow" or "thinkertoy".
 	} else {
-		fmt.Print("ERROR\n", "Cannot read the input arguments")
+		fmt.Print("ERROR.\n", "Usage: go run . [STRING] [BANNER] || Example: go run . \"test\" standard or shadow or thinkertoy ")
+		return
 	}
 
-	argStr := strings.Split(os.Args[1], "\n")
+	sepArgs := strings.Split(argStr, "\\n")
 
-	fmt.Println(argStr)
+	////////////////////////////////
+	//# Task 2: Read Banner Files//
+	//////////////////////////////
+
+	file, err := os.ReadFile("assets/" + banner + ".txt") // read the "banner name. Marge it with ".txt" and read the file
+	if err != nil {
+		fmt.Println("ERROR\n", banner+"Does not exist.")
+		return
+	}
+
+	lines := strings.Split(string(file), "\n")
+
+	PrintAsciiArt(sepArgs, lines)
+}
+
+func PrintAsciiArt(word []string, textfile []string) {
 }
